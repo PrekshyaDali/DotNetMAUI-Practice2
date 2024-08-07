602b6f4f4d96a335fa79ea7e897971212631baf4
Prekshya <prekshya.dali@bajratechnologies.com>
2024-08-07 08:22:31 +0545

    feat: add MAUI practice file: Unit test

---
+++ Models/ArithmeticServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrekshyaMaui.Models
+{
+    public class ArithmeticServices
+    {
+
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new ArgumentException("You can't divide by zero");
+            return (double)a / b;
+        }
+    }
+}
